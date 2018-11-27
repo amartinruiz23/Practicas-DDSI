22d9af8022564c25c5eeaa47644570489d64a22a
José María Martín <jmmartinluque@gmail.com>
2018-11-27 13:11:11 +0100

    Unificado archivos y corregido trigger

--- src/trigger.sql
+++ src/trigger.sql
@@ -1,3 +1,20 @@
+-- Trigger de José María Martín Luque
+CREATE TRIGGER crear_cancion
+  BEFORE INSERT ON cancion
+  FOR EACH ROW
+BEGIN
+  IF NEW.autor NOT IN (
+	  SELECT identificador FROM autor
+	  WHERE identificador = NEW.autor
+  ) OR NEW.genero NOT IN (
+    SELECT nombre FROM genero
+	  WHERE nombre = NEW.genero
+  ) THEN
+    SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'La canción añadida debe tener un autor y género de entre los existentes';
+  END IF;
+END; 
+
+-- Trigger de Luis Antonio Ortega Andrés
 CREATE TRIGGER email.usuario
   BEFORE INSERT ON usuario
   FOR EACH ROW
